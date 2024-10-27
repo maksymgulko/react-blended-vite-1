1d807fc76ff4a2f3e2ed14767fdac96094834c1b
SerhiiDziuba1309 <doktorschuuu@gmail.com>
2024-10-27 11:31:41 +0100

    task 2

--- src/components/StatisticsItem/StatisticsItem.jsx
+++ src/components/StatisticsItem/StatisticsItem.jsx
@@ -1,3 +1,11 @@
-export const StatisticsItem = () => {
-  return <h3>StatisticsItem</h3>;
+import style from './Statistics.module.css';
+
+export const StatisticsItem = ({ item, icon: Icon }) => {
+  return (
+    <>
+      <Icon size={40} />
+      <span className={style.counter}>{item.total}</span>
+      <p className={style.text}>{item.title}</p>
+    </>
+  );
 };
